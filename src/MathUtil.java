b4fc23c5e0eef6bf0081d82191f14b10c9861c20
Atak Oskonbaev <atakoskonbaev21@gmail.com>
2024-09-28 13:45:58 +0600

    added repo

---
+++ src/MathUtil.java
@@ -0,0 +1,47 @@
+public class MathUtil {
+    public static boolean isPrime(int n)
+    {
+        return true;
+    }
+
+    public static int gcd(int a, int b)
+    {
+        return b;
+    }
+
+    public static int lcm(int a, int b)
+    {
+        return a;
+    }
+
+    public static int fibonacci(int n)
+    {
+        return n;
+    }
+
+    public static int factorial(int n)
+    {
+        return n;
+    }
+
+
+    public boolean isPerfectNumber(int n)
+    {
+        return true;
+    }
+
+    public int sumOfDigits(int n)
+    {
+        return n;
+    }
+
+    public boolean isArmstrongNumber(int n)
+    {
+        return true;
+    }
+
+    public int nextPrime(int n)
+    {
+        return n;
+    }
+}

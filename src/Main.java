66700bf9f31c6437f713ce5a46368626b566d234
Atak Oskonbaev <atakoskonbaev21@gmail.com>
2024-09-28 15:25:58 +0600

    added methods

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,61 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        //five static methods
+        //isPrime()
+        System.out.println(MathUtil.isPrime(23));  //true
+        System.out.println(MathUtil.isPrime(11));  //true
+        System.out.println(MathUtil.isPrime(100));  //false
 
-        for (int i = 1; i <= 5; i++) {
+        //gcd()
+        System.out.println((MathUtil.gcd(21, 7))); //7
+        System.out.println((MathUtil.gcd(98, 56)));  //14
+        System.out.println((MathUtil.gcd(0, 100)));  //0
 
-            System.out.println("i = " + i);
-        }
+        //lcm()
+        System.out.println(MathUtil.lcm(15, 25));  //75
+        System.out.println(MathUtil.lcm(10, 3));  //30
+        System.out.println(MathUtil.lcm(17, 15));  //255
+
+        //fibonacci()
+        MathUtil.fibonacci(2);  //0 1
+        MathUtil.fibonacci(5);  //0 1 1 2 3
+        MathUtil.fibonacci(7);  //0 1 1 2 3 5 8
+
+
+        //factorial()
+        System.out.println(MathUtil.factorial(3));  //6
+        System.out.println(MathUtil.factorial(8));  //40320
+        System.out.println(MathUtil.factorial(10));  //3628800
+
+
+        //five non-static methods
+        MathUtil mathUtil = new MathUtil();
+
+        //isPerfectNumber()
+        System.out.println(mathUtil.isPerfectNumber(6));  //true
+        System.out.println(mathUtil.isPerfectNumber(28));  //true
+        System.out.println(mathUtil.isPerfectNumber(23));  //false
+
+        //sumOfDigits()
+        System.out.println(mathUtil.sumOfDigits(123));  //6
+        System.out.println(mathUtil.sumOfDigits(5123));  //11
+        System.out.println(mathUtil.sumOfDigits(64));  //10
+
+        //reverseNumber()
+        System.out.println(mathUtil.reverseNumber(64));  //46
+        System.out.println(mathUtil.reverseNumber(6479));  //6479
+        System.out.println(mathUtil.reverseNumber(34));  //43
+
+        //isArmstrongNumber()
+        System.out.println(mathUtil.isArmstrongNumber(153));  //true
+        System.out.println(mathUtil.isArmstrongNumber(123));  //false
+        System.out.println(mathUtil.isArmstrongNumber(5));  //true
+
+        //nextPrime()
+        System.out.println(mathUtil.nextPrime(11));  //13
+        System.out.println(mathUtil.nextPrime(560));  //563
+        System.out.println(mathUtil.nextPrime(34));  //37
+
     }
 }
--- src/MathUtil.java
+++ src/MathUtil.java
@@ -1,47 +1,147 @@
+import java.lang.reflect.Array;
+
 public class MathUtil {
+
+    //five static methods
     public static boolean isPrime(int n)
     {
+        if(n == 1) //check if n = 1
+        {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false; // n is divisible by i, so it is not prime
+            }
+        }
         return true;
     }
 
     public static int gcd(int a, int b)
     {
-        return b;
+        int result;
+        if(a == 0 || b == 0) //check for a or b = 0
+        {
+            return 0;
+        }
+        //find the min of a and b
+        result = Math.min(a, b);
+        for(int i = result; i > 0; i--)
+        {
+            if(a % result == 0 && b % result == 0) //break if both are dividable
+            {
+                break;
+            }
+            result--;
+        }
+        return result;
     }
 
     public static int lcm(int a, int b)
     {
-        return a;
+        int result;
+        if (a > b) //find the largest
+        {
+            result = a;
+        }
+        else
+        {
+            result = b;
+        }
+        for (int i = result; i > 0; i++)
+        {
+            if (result % a == 0 & result % b == 0) //if the largest dividable by both, return largest
+            {
+                break;
+            }
+            result++; //if not, make it +1
+        }
+        return result;
     }
 
-    public static int fibonacci(int n)
+    public static void fibonacci(int n)
     {
-        return n;
+        int a = 0, b = 1; // starting values for Fibonacci
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            int next = a + b; // calculate the next term
+            a = b; // update a to the next term
+            b = next; // update b to the next term
+        }
+        System.out.println();
     }
 
     public static int factorial(int n)
     {
-        return n;
+        int result = 1;
+        for (int i = 1; i <= n; i++) //multiply all numbers from 1 to n
+        {
+            result *= i;
+        }
+        return result;
     }
 
-
+    //five non-static methods
     public boolean isPerfectNumber(int n)
     {
-        return true;
+        int result = 0;
+        for (int i = 1; i <= n/2; i++) //check divisors from 1 to n/2
+        {
+            if (n % i == 0)
+            {
+                result += i; //add to the result
+            }
+        }
+        return result == n;
     }
 
     public int sumOfDigits(int n)
     {
-        return n;
+        int result = 0;
+        while(n >= 10) //add till the last digit
+        {
+            result += n % 10;
+            n /= 10;
+        }
+
+        return result + n; //add the last digit
+    }
+
+    public int reverseNumber(int n)
+    {
+        int reversed = 0;
+        while (n != 0) {
+            int digit = n % 10;          // get the last digit
+            reversed = reversed * 10 + digit; // add it to the reversed number
+            n /= 10;
+        }
+        return reversed;
     }
 
     public boolean isArmstrongNumber(int n)
     {
-        return true;
+        int originalNumber = n;
+        int sum = 0;
+        int digits = String.valueOf(n).length(); // count the number of digits
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += (int) Math.pow(digit, digits); // raise it to the power of digits and add to sum
+            n /= 10;
+        }
+
+        return sum == originalNumber;
     }
 
     public int nextPrime(int n)
     {
+        n++;
+
+        while (!isPrime(n)) {
+            n++;
+        }
+
         return n;
     }
 }
